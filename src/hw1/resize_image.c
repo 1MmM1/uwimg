a367dc85ceb4d960ce5925afd72bd68ada444567
1MmM1 <lin-family@live.com>
2021-04-08 00:37:06 -0700

    Pass tests for nn_interpolate; initial version of nn_resize

--- src/hw1/resize_image.c
+++ src/hw1/resize_image.c
@@ -4,13 +4,25 @@
 float nn_interpolate(image im, float x, float y, int c)
 {
     // TODO Fill in
-    return 0;
+    return get_pixel(im, round(x), round(y), c);
 }
 
 image nn_resize(image im, int w, int h)
 {
     // TODO Fill in (also fix that first line)
-    return make_image(1,1,1);
+    image resized = make_image(w, h, im.c);
+    float x_scale = im.w / resized.w;
+    float y_scale = im.h / resized.h;
+    float x_start = -fabs(im.w - resized.w) / 2;
+    float y_start = -fabs(im.h - resized.h) / 2;
+    for (int i = 0; i < resized.w; i++) {
+        for (int j = 0; j < resized.h; j++) {
+            for (int k = 0; k < resized.c; k++) {
+                set_pixel(resized, i, j, k, nn_interpolate(im, x_start + i * x_scale, y_start + j * y_scale, k));
+            }
+        }
+    }
+    return resized;
 }
 
 float bilinear_interpolate(image im, float x, float y, int c)
