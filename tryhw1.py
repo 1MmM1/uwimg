78a1723623b28714b4c9a8d5adc03c7c06702fcc
1MmM1 <lin-family@live.com>
2021-04-13 11:24:46 -0700

    Add code to generate images for bilinear upscale and resizing down

--- tryhw1.py
+++ tryhw1.py
@@ -2,3 +2,18 @@ from uwimg import *
 im = load_image("data/dogsmall.jpg")
 a = nn_resize(im, im.w*4, im.h*4)
 save_image(a, "dog4x-nn")
+
+# Bilinear resize up
+im = load_image("data/dogsmall.jpg")
+a = bilinear_resize(im, im.w*4, im.h*4)
+save_image(a, "dog4x-bl")
+
+# NN resize down
+im = load_image("data/dog.jpg")
+a = nn_resize(im, im.w//7, im.h//7)
+save_image(a, "dog7th-nn")
+
+# Bilinear resize down
+im = load_image("data/dog.jpg")
+a = bilinear_resize(im, im.w//7, im.h//7)
+save_image(a, "dog7th-bl")
