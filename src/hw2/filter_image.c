37af102092538f4a8a4e1afd3a7d8104fa7102b6
1MmM1 <lin-family@live.com>
2021-04-19 10:33:44 -0700

    Initial implementation of l1_normalize, make_box_filter, and convolve_image

--- src/hw2/filter_image.c
+++ src/hw2/filter_image.c
@@ -9,18 +9,85 @@
 void l1_normalize(image im)
 {
     // TODO
+    float sum = 0;
+    for (int i = 0; i < im.w; i++) {
+        for (int j = 0; j < im.h; j++) {
+            for (int k = 0; k < im.c; k++) {
+                sum += get_pixel(im, i, j, k);
+            }
+        }
+    }
+    for (int i = 0; i < im.w; i++) {
+        for (int j = 0; j < im.h; j++) {
+            for (int k = 0; k < im.c; k++) {
+                set_pixel(im, i, j, k, get_pixel(im, i, j, k) / sum);
+            }
+        }
+    }
 }
 
 image make_box_filter(int w)
 {
     // TODO
-    return make_image(1,1,1);
+    image im = make_image(w, w, 1);
+    for (int i = 0; i < im.w; i++) {
+        for (int j = 0; j < im.h; j++) {
+            set_pixel(im, i, j, 0, 1.0 / w);
+        }
+    }
+    return im;
 }
 
 image convolve_image(image im, image filter, int preserve)
 {
     // TODO
-    return make_image(1,1,1);
+    assert(filter.c == 1 || filter.c == im.c);
+    image result = make_image(1,1,1);
+    if (filter.c == im.c && preserve != 1) {
+        result = make_image(im.w, im.h, 1);
+        for (int i = 0; i < im.w; i++) {
+            for (int j = 0; j < im.h; j++) {
+                // apply filter 1:1 to image and sum all channels
+                int sum = 0;
+                for (int q = 0; q < filter.w; q++) {
+                    for (int r = 0; r < filter.h; r++) {
+                        for (int s = 0; s < filter.c; s++) {
+                            sum += get_pixel(filter, q, r, s) * get_pixel(im, i - filter.w / 2 + q, j - filter.h / 2 + r, s);
+                        }
+                    }
+                }
+                set_pixel(result, i, j, 0, sum);
+            }
+        }
+    } else if (filter.c == im.c && preserve == 1) {
+        result = make_image(im.w, im.h, im.c);
+        for (int i = 0; i < im.w; i++) {
+            for (int j = 0; j < im.h; j++) {
+                for (int k = 0; k < im.c; k++) {
+                    // apply filter 1:1 to image and each channel individually
+                    int sum = 0;
+                    for (int q = 0; q < filter.w; q++) {
+                        for (int r = 0; r < filter.h; r++) {
+                            sum += get_pixel(filter, q, r, k) * get_pixel(im, i - filter.w / 2 + q, j - filter.h / 2 + r, k);
+                        }
+                    }
+                    set_pixel(result, i, j, k, sum);
+                }
+            }
+        }
+    } else if (filter.c == 1) {
+        for (int i = 0; i < im.w; i++) {
+            for (int j = 0; j < im.h; j++) {
+                for (int k = 0; k < im.c; k++) {
+                    
+                }
+            }
+        }
+    } else {
+        // Should never get here
+        printf("Something went wrong applying convolutions.");
+    }
+    return result;
 }
 
 image make_highpass_filter()
