39368cabf8688c2eac7e14642b37dc89e332ca5e
1MmM1 <lin-family@live.com>
2021-04-20 10:39:23 -0700

    Add post processing function to fix underflow and overflow issues

--- tryhw2.py
+++ tryhw2.py
@@ -8,19 +8,29 @@ save_image(blur, "dog-box7")
 thumb = nn_resize(blur, blur.w//7, blur.h//7)
 save_image(thumb, "dogthumb")
 
+def optimize_for_display(image):
+    for i in range(image.w):
+        for j in range(image.h):
+            for k in range(image.c):
+                val = min(max(0, get_pixel(image, i, j, k)), 1);
+                set_pixel(image, i, j, k, val);
+
 # Use highpass filter
 f = make_highpass_filter()
 highpass = convolve_image(im, f, 0)
+optimize_for_display(highpass)
 save_image(highpass, "dog-highpass")
 
 # Use sharpen filter
 f = make_sharpen_filter()
 sharpen = convolve_image(im, f, 1)
+optimize_for_display(sharpen)
 save_image(sharpen, "dog-sharpen")
 
 # Use emboss filter
 f = make_emboss_filter()
 emboss = convolve_image(im, f, 1)
+optimize_for_display(emboss)
 save_image(emboss, "dog-emboss")
 
 # Use Gaussian filter
